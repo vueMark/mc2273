3cca09671891cac743cdce0414b08042659096dc
mark.costales <mcostales@t2s-solutions.com>
2021-01-21 21:10:10 -0500

    placeholder commit message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="visa">
+    <h1>Vue Visa Application</h1>
+    <form>
+      <label for="first name">First Name:</label>
+      <input type="text" required>
+      <br>
+      <label for="last name">Last Name:</label>
+      <input type="text" required>
+      <br>
+      <label for="country">Nationality:</label>
+      <input type="text"  required>
+      <br>
+      <label for="passport number">Passport Number:</label>
+      <input type="text"  required>
+
+      <label for="duration">Duration of stay:</label>
+      <input type="text"  required>
+     <br><br>
+      <button @click="addVisa">Add another previous visa</button>
+      <br>
+      <div class="previous"
+      v-for="(applicant, counter) in applicants"
+      v-bind:key="counter">
+        <span @click="deleteVisa(counter)">x</span>
+        <label for="duration">{{counter+1}}. Previous Visa:</label>
+        <input type="text" v-model.lazy="applicant.previous" required>
+        <label for="duration">Year of expiration:</label>
+        <input type="text" v-model.lazy="applicant.expiration" required>
+      </div>
+    </form>
+
   </div>
 </template>
 
@@ -35,24 +38,69 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+       applicants:[
+       {
+      previous: '',
+      expiration:''
+       }
+     ]
+    }
+  },
+  methods : {
+    addVisa(){
+      this.applicants.push({
+        previous:'',
+        expiration: ''
+      })
+    },
+    deleteVisa(counter){
+      this.applicants.splice(counter,1);
+
+    }
   }
 }
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#visa {
+  margin: 20px auto;
+  max-width: 700px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+label{
+  display: block;
+  margin: 20px 0 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+input {
+  font-size: 30px;
+  border: 1px double rgb(102, 97, 96) ;
+  border-radius: 4px;
 }
-a {
-  color: #42b983;
+button {
+  font-size: 16px;
+ background: rgb(64, 179, 140);
+  padding: 0.4rem 1.3rem;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+ margin: 10px;
+}
+span{
+  width: 30px;
+  float: right;
+  cursor: pointer;
+}
+span:hover{
+  color: brown;
+}
+.previous{
+  border: 1.5px solid;
+  padding:5px;
+  margin-bottom: 10px;
 }
 </style>
